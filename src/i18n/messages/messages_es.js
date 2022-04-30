835c30136c8ebff53c85422360e8921c226ff965
David GG <david.ggondell@udc.es>
2022-04-30 18:21:32 +0200

    changed en for es

--- src/i18n/messages/messages_es.js
+++ src/i18n/messages/messages_es.js
@@ -13,20 +13,17 @@ export default {
 
     'project.startLoader.title': 'CAPTURANDO INSTANTES',
 
-    'project.aboutme.text.first': 'A Estrada (1964).'
-        + ' Photography has always been present in my life. My mother had a'
-        + ' simple Kodak instamatic with which I played to emulate my neighbour, the great'
-        + ' photojournalist from Coruña Xosé Castro (Pepucho). I felt a special attraction every'
-        + ' time I saw him arrive with all his photographic equipment. I loved touching those'
-        + ' cameras or watching him at any corner of our city doing his job. I definitely think that it'
-        + ' was him who marked my interest in this hobby.',
-    'project.aboutme.text.second': 'When I was 17 years old, I got my first reflex camera, a simple Russian-made Practika'
-        + ' BX20 with which I started making slides and photographs in black and white. Five years'
-        + ' later, I had the opportunity to start working doing studio reports and social photography'
-        + ' using a medium format camera Zenza Bronica and a reflex Nikon FM2.',
-    'project.aboutme.text.third': 'Nowadays I dedicate my free time to show the ethnographic and landscape heritage'
-        + ' mainly of my land (Galicia), and photography is still my great hobby, the one which has accompanied'
-        + ' me over the years. It is an escape valve with which I enjoy like the first day.',
+    'project.aboutme.text.first': 'A Estrada (1964)'
+        + ' La fotografía siempre ha estado presente en mi vida. Mi madre tenía una sencilla Kodak instamatic con la jugaba a emular a mi vecino,'
+        + ' el gran fotoperiodista coruñés Xosé Castro (Pepucho). Sentía una especial atracción cada vez que lo veía llegar con todo su equipo y'
+        + ' me fascinaba tocar aquellas cámaras o verlo por algún rincón de la ciudad realizando su trabajo. Creo que fue él quien, definitivamente,'
+        + ' marcó mi interés por esta afición.',
+    'project.aboutme.text.second': 'A la edad de 17 años conseguí mi primera cámara réflex, una sencilla Practika BX20 de fabricación rusa, con la que empecé a'
+        + ' hacer mis primeras diapositivas y fotografías en blanco y negro. Cinco años mas tarde tuve la oportunidad de comenzar a'
+        + ' trabajar realizando reportajes de estudio y fotografía social usando una cámara de formato medio Zenza Bronica y una réflex Nikon FM2.',
+    'project.aboutme.text.third': 'En la actualidad dedico mi tiempo libre a mostrar el patrimonio etnográfico y paisajístico principalmente de mi tierra (Galicia)'
+        + ' y la fotografía sigue siendo mi gran afición que me ha acompañado a lo largo de los años. Una válvula de escape con la que'
+        + ' disfruto como el primer día.',
 
     'project.contact.contact': 'Contacto',
     'project.contact.legalInfo': 'Información legal',
