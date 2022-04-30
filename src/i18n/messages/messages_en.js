3d783ee624827b71b37b99a1e9d52d560913544f
David GG <david.ggondell@udc.es>
2022-04-30 16:01:25 +0200

    texto corregido en español e inglés

--- src/i18n/messages/messages_en.js
+++ src/i18n/messages/messages_en.js
@@ -15,14 +15,14 @@ export default {
 
     'project.aboutme.text.first': 'A Estrada (1964)'
         + ' La fotografía siempre ha estado presente en mi vida. Mi madre tenía una sencilla Kodak instamatic con la jugaba a emular a mi vecino,'
-        + ' el gran fotoperiodista coruñés Xosé Castro (Pepucho). Sentía una especial atracción cada vez que lo veía llegar con todo su equipo,'
-        + ' me fascinaba tocar aquellas cámaras o verlo por algún rincón de la ciudad realizando su trabajo. Creo que fue él quien definitivamente'
+        + ' el gran fotoperiodista coruñés Xosé Castro (Pepucho). Sentía una especial atracción cada vez que lo veía llegar con todo su equipo y'
+        + ' me fascinaba tocar aquellas cámaras o verlo por algún rincón de la ciudad realizando su trabajo. Creo que fue él quien, definitivamente,'
         + ' marcó mi interés por esta afición.',
     'project.aboutme.text.second': 'A la edad de 17 años conseguí mi primera cámara réflex, una sencilla Practika BX20 de fabricación rusa, con la que empecé a'
         + ' hacer mis primeras diapositivas y fotografías en blanco y negro. Cinco años mas tarde tuve la oportunidad de comenzar a'
-        + ' trabajar realizando trabajos de estudio y fotografía social usando una cámara de formato medio Zenza Bronica y Nikon FM2.',
+        + ' trabajar realizando reportajes de estudio y fotografía social usando una cámara de formato medio Zenza Bronica y una réflex Nikon FM2.',
     'project.aboutme.text.third': 'En la actualidad dedico mi tiempo libre a mostrar el patrimonio etnográfico y paisajístico principalmente de mi tierra (Galicia)'
-        + ' y la fotografía sigue siendo mi gran afición que me ha acompañado a lo largo de los años, una válvula de escape con la que'
+        + ' y la fotografía sigue siendo mi gran afición que me ha acompañado a lo largo de los años. Una válvula de escape con la que'
         + ' disfruto como el primer día.',
 
     'project.contact.contact': 'Contact',
--- src/i18n/messages/messages_gl.js
+++ src/i18n/messages/messages_gl.js
@@ -15,14 +15,14 @@ export default {
 
     'project.aboutme.text.first': 'A Estrada (1964)'
         + ' A fotografía seempre estivo presente na miña vida. A miña nai tiña unha sinxela Kodak instamatic coa que xogaba a emular ao meu veciño,'
-        + ' o gran fotoperiodista coruñés Xosé Castro (Pepucho). Sentía unha especial atracción cada vez que o vía chegar con todo o seu equipo,'
+        + ' o gran fotoperiodista coruñés Xosé Castro (Pepucho). Sentía unha especial atracción cada vez que o vía chegar con todo o seu equipo e'
         + ' fascinábame tocar aquelas cámaras ou velo por algún rincón da cidade realizando o seu traballo. Creo que foi él quen definitivamente'
         + ' marcou o meu interés por esta afección.',
     'project.aboutme.text.second': 'Á idade de 17 anos conseguín a miña primeira cámara réflex, unha sinxela Practika BX20 de fabricación rusa, coa que comecei a'
         + ' facer as miñas primeiras diapositivas e fotografías en branco e negro. Cinco anos máis tarde tiven a oportunidade de comezar a'
-        + ' traballar realizando traballos de estudio e fotografía social usando unha cámara de formato medio Zenza Bronica e Nikon FM2.',
+        + ' traballar realizando reportaxes de estudio e fotografía social usando unha cámara de formato medio Zenza Bronica e unha réflex Nikon FM2.',
     'project.aboutme.text.third': 'Na actualidade dedico o meu tempo libre a mostrar o patrimonio etnográfico e paisaxístico principalmente da miña terra (Galicia)'
-        + ' e a fotografía segue sendo a miña gran afección que me acompañou ao longo dos anos, unha válvula de escape coa que'
+        + ' e a fotografía segue sendo a miña gran afección que me acompañou ao longo dos anos. Unha válvula de escape coa que'
         + ' disfruto como o primeiro día.',
 
     'project.contact.contact': 'Contacto',
